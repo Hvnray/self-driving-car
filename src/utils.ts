9f66100c7140021e9f4ede6d401100e33ca65938
hvnray <slenzy001@gmail.com>
2022-07-26 15:40:51 +0100

    add getIntersection formular to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { BorderPostions, BorderPostionsAndOffset } from "./types";
+
 /**
  * Lerp - `Linear Interpolation Util`
  * @param A left point
@@ -7,4 +9,27 @@
  */
 export function lerp(A: number, B: number, t: number): number {
   return A + (B - A) * t;
+}
+export function getIntersection(
+  A: BorderPostions,
+  B: BorderPostions,
+  C: BorderPostions,
+  D: BorderPostions
+): BorderPostionsAndOffset | null {
+  const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+  const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
+  const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
+
+  if (bottom != 0) {
+    const t = tTop / bottom;
+    const u = uTop / bottom;
+    if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+      return {
+        x: lerp(A.x, B.x, t),
+        y: lerp(A.y, B.y, t),
+        offset: t,
+      };
+    }
+  }
+  return null;
 }
