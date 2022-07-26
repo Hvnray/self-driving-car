f6d49cea94feff6c78c42436401c4ca52cbceeed
hvnray <slenzy001@gmail.com>
2022-07-26 15:40:24 +0100

    move all types and interfaces to a one module

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+export interface CarConfig {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+/**
+ * Represents the border
+ */
+export interface BorderPostions {
+  /**`x` represents the horizontal axis, left - right  */
+  x: number;
+  /**`y` represents the vertical axis, top - bottom  */
+  y: number;
+}
+
+export type BorderPostionsTuple = [BorderPostions, BorderPostions];
+
+export type BordersSections = BorderPostionsTuple[];
+
+export type BorderPostionsAndOffset = BorderPostions & { offset: number };
+
+export type BorderPostionsAndOffsetOrNull = BorderPostionsAndOffset | null;
