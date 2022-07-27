6079897b59a6b29c5025fc5f2366f8b44159525b
hvnray <slenzy001@gmail.com>
2022-07-27 12:10:17 +0100

    add polygon intersection function and documention

--- src/utils.ts
+++ src/utils.ts
@@ -13,18 +13,18 @@ export function lerp(A: number, B: number, t: number): number {
 
 /**
  * getIntersection - `Line Intersection Util`
- * @param {BorderPostions} A point lines 
- * @param {BorderPostions} B point lines 
- * @param {BorderPostions} C point lines 
- * @param {BorderPostions} D point lines 
+ * @param {BorderPostions} A point lines
+ * @param {BorderPostions} B point lines
+ * @param {BorderPostions} C point lines
+ * @param {BorderPostions} D point lines
  * @returns {OrNull<BorderPostionsAndOffset>} Linear Interpolation `OrNull<BorderPostionsAndOffset>`
  */
-export function getIntersection(
-  A: BorderPostions,
-  B: BorderPostions,
-  C: BorderPostions,
-  D: BorderPostions
-): OrNull<BorderPostionsAndOffset> {
+export function getIntersection<
+  A extends BorderPostions,
+  B extends BorderPostions,
+  C extends BorderPostions,
+  D extends BorderPostions
+>(A: A, B: B, C: C, D: D): OrNull<BorderPostionsAndOffset> {
   const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
   const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
   const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
@@ -41,4 +41,29 @@ export function getIntersection(
     }
   }
   return null;
+}
+
+/**
+ * Takes two polygon like points and checks if the intersect (touch)
+ * @implements `getIntersection` function
+ * @param {Type1 extends BorderPostions} polygon1
+ * @param {Type2 extends BorderPostions}polygon2
+ * @returns {boolean} true if polygon lines touch, else false
+ */
+export function polygonsIntersect<
+  Type1 extends BorderPostions,
+  Type2 extends BorderPostions
+>(polygon1: Type1[], polygon2: Type2[]): boolean {
+  for (let i = 0; i < polygon1.length; i++) {
+    for (let j = 0; j < polygon2.length; j++) {
+      const touch = getIntersection(
+        polygon1[i],
+        polygon1[(i + 1) % polygon1.length],
+        polygon2[j],
+        polygon2[(j + 1) % polygon2.length]
+      );
+      if (touch) return true;
+    }
+  }
+  return false;
 }
